70686435a556895b7ca710d99784e00c882c82c9
Connor <connor.morrison@uqconnect.edu.au>
2024-06-16 21:02:05 +1000

    modified tailwind.config and installed craco, idk if I need it fireship said so though

--- portfolio-front-end/tailwind.config.js
+++ portfolio-front-end/tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-        colors: {
-            white: '#FFFFFF',
-            blue: '#0077CC',
-            black: '#000000',
-            dark: '#1E1E1E',
-            lightGrey: '#D9D9D9',
-            grey: '#2E2F2F'
-          },
+    mode: 'jit',
+    purge: ['.src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                white: '#FFFFFF',
+                blue: '#0077CC',
+                black: '#000000',
+                dark: '#1E1E1E',
+                lightGrey: '#D9D9D9',
+                grey: '#2E2F2F'
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
