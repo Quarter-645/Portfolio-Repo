01179a6a01d075487e19761455027513433f6e77
Connor <connor.morrison@uqconnect.edu.au>
2024-06-19 20:28:08 +1000

    not sure why but the profile picture was blocking a git checkout

--- game-service/game_service/email/EmailHandler.py
+++
@@ -1,9 +0,0 @@
-from EmailFormatter import * 
-from EmailSender import *
-
-
-def handleEmail(request):
-    formatter = EmailFormatter(request)
-    sender = EmailSender(formatter.formatEmailBody)
-    
-    sender.send_email()
