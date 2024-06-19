dd8c76b2ffedc46dcb187582b321c61276c252fc
Connor <connor.morrison@uqconnect.edu.au>
2024-06-19 20:05:27 +1000

    added email logic to back-end, possible now to send an email via a POST request, needs testing though

--- game-service/game_service/views/routes.py
+++ game-service/game_service/views/routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, jsonify, request 
+from email.EmailHandler import handleEmail
 
 api = Blueprint('api', __name__, url_prefix='/api/')
 
 @api.route('/health')
 def health():
     return jsonify({"status": "ok"}), 200
+
+
+@api.route('/handle_email', methods=['POST'])
+def handleEmail():
+    handleEmail(request)    
+    return jsonify({"status" : "Email Sent!"}), 201
