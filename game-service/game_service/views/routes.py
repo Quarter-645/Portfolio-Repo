fc838450cebc35bef331caa21ccd275e51f28d7a
Connor <connor.morrison@uqconnect.edu.au>
2024-06-30 16:27:41 +1000

    updated game service to work with CORS

--- game-service/game_service/views/routes.py
+++ game-service/game_service/views/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, request 
-from email.EmailHandler import handleEmail
 
 api = Blueprint('api', __name__, url_prefix='/api/')
 
@@ -8,7 +7,7 @@ def health():
     return jsonify({"status": "ok"}), 200
 
 
-@api.route('/handle_email', methods=['POST'])
-def handleEmail():
-    handleEmail(request)    
-    return jsonify({"status" : "Email Sent!"}), 201
+# @api.route('/handle_email', methods=['POST'])
+# def handleEmail():
+#     handleEmail(request)    
+#     return jsonify({"status" : "Email Sent!"}), 201
--- game-service/run.py
+++ game-service/run.py
@@ -1,6 +1,8 @@
 from game_service import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
