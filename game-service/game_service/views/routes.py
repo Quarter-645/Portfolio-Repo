fdc54956c3feb01fae57f0b5da8c5062ed875978
Connor <connor.morrison@uqconnect.edu.au>
2024-07-13 10:57:28 +1000

    updated api paths so that /api/ isn't included in the url

--- game-service/game_service/views/routes.py
+++ game-service/game_service/views/routes.py
@@ -3,7 +3,7 @@ from game_service.email.EmailFormatter import EmailFormatter
 from game_service.email.EmailSender import EmailSender
 from game_service.extensions.LimiterExtension import limiter
 
-api = Blueprint('api', __name__, url_prefix='/api/')
+api = Blueprint('api', __name__)
 
 @api.route('/health')
 @limiter.limit("1/second")
